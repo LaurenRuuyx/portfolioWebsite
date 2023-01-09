ab89adc76396b77a7d58f3a7acbb820309c8e67c
LaurenRuuyx <98824446+LaurenRuuyx@users.noreply.github.com>
2023-01-09 21:18:12 +0000

    cubestuff

--- portfoliowebsite/src/App.js
+++ portfoliowebsite/src/App.js
@@ -2,6 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  window.addEventListener('scroll', () => {
+    document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+  }, false);
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +13,18 @@ function App() {
         <br></br>
         Software Developer
       </header>
+    <div class="progress"></div>
+      <div class="cube-wrap">
+      <div class="cube">
+      <div class="side top"></div>
+      <div class="side bottom"></div>
+      <div class="side front"></div>
+      <div class="side back"></div>
+      <div class="side left"></div>
+      <div class="side right"></div>
+      </div>
+    </div>
+
       <div className="spacing-div">
       </div>
       <div className="lightsDiv">
